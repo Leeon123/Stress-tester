f79f5d32db0b1fb341abe0b64ab394ec6ce0203e
TaTal-Bet <95242884+TaTal-Bet@users.noreply.github.com>
2022-03-02 05:48:31 +0330

    Update stress.go

--- stress.go
+++ stress.go
@@ -22,6 +22,37 @@ import (
 )
 
 //Start of Random useragent
+import requests
+import random
+from bs4 import BeautifulSoup as bs
+
+def get_free_proxies():
+    url = "https://free-proxy-list.net/"
+    # get the HTTP response and construct soup object
+    soup = bs(requests.get(url).content, "html.parser")
+    proxies = []
+    for row in soup.find("table", attrs={"id": "proxylisttable"}).find_all("tr")[1:]:
+        tds = row.find_all("td")
+        try:
+            ip = tds[0].text.strip()
+            port = tds[1].text.strip()
+            host = f"{ip}:{port}"
+            proxies.append(host)
+        except IndexError:
+            continue
+    return proxies
+proxies = [
+    '167.172.248.53:3128',
+    '194.226.34.132:5555',
+    '203.202.245.62:80',
+    '141.0.70.211:8080',
+    '118.69.50.155:80',
+    '201.55.164.177:3128',
+    '51.15.166.107:3128',
+    '91.205.218.64:80',
+    '128.199.237.57:8080',
+]
+
 var (
 	str       string = "asdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM=&"
 	succ             = 0
